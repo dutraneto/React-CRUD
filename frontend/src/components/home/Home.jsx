5898cf591849b26a273a9a8b7d2905f6d1ab4c6d
dutraneto <dudutraneto@gmail.com>
2019-02-11 22:33:17 -0500

    add component Home

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -4,3 +4,10 @@ import './Home.css'
 import Main from '../template/Main'
 
 export default props =>
+    <Main icon="home" title="start" subtitle="2nd Project of React Studies">
+        <div className="display-4">Welcome!</div>
+        <hr/>
+        <p className="mb-0">The porpuse of this system is to exemplify
+            a register developed using React.
+        </p>
+    </Main>
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -5,20 +5,13 @@ import React from 'react'
 
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-import NavItem from '../components/template/NavItem'
-import Main from '../components/template/Main'
+import Home from '../components/home/Home'
 import Footer from '../components/template/Footer'
 
 export default props => 
     <div className="app">
         <Logo />
         <Nav icon="home" info="info" contact="envelope-o" blog="globe" careers="star-o" users="users"/>
-        <Main icon="home" title="start" subtitle="2nd Project of React Studies">
-            <div className="display-4">Welcome!</div>
-            <hr/>
-            <p className="mb-0">The porpuse of this system is to exemplify
-                a register developed using React.
-            </p>
-        </Main>
+        <Home />
         <Footer />
     </div>
