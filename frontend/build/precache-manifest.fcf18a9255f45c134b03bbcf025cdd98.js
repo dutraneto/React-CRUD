0c403e24b5f6c4a93eb4002a3381d4bf62e19a17
dutraneto <dudutraneto@gmail.com>
2019-02-12 08:22:31 -0500

    re-build

---
+++ frontend/build/precache-manifest.fcf18a9255f45c134b03bbcf025cdd98.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "89e2666c24d37055bcb60e9d2d9f7e35",
+    "url": "/react-crud/frontend/build/static/media/Roboto-Thin.89e2666c.ttf"
+  },
+  {
+    "revision": "842b3a37222f5089c88a",
+    "url": "/react-crud/frontend/build/static/css/main.e9f53826.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-crud/frontend/build/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "40d1de3423d2c27f288a",
+    "url": "/react-crud/frontend/build/static/js/1.40d1de34.chunk.js"
+  },
+  {
+    "revision": "adf70f5872511ef6a16b",
+    "url": "/react-crud/frontend/build/static/js/runtime~main.adf70f58.js"
+  },
+  {
+    "revision": "8cd1c759964a6c1f0e25a86cbc4fa6da",
+    "url": "/react-crud/frontend/build/static/media/logo.8cd1c759.png"
+  },
+  {
+    "revision": "842b3a37222f5089c88a",
+    "url": "/react-crud/frontend/build/static/js/main.842b3a37.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-crud/frontend/build/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-crud/frontend/build/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-crud/frontend/build/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-crud/frontend/build/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "40d1de3423d2c27f288a",
+    "url": "/react-crud/frontend/build/static/css/1.efcf7da0.chunk.css"
+  },
+  {
+    "revision": "98f252bf856fbc29605d1516d90cd18e",
+    "url": "/react-crud/frontend/build/index.html"
+  }
+];
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
+// eslint-disable-next-line
 import { HashRouter, BrowserRouter } from 'react-router-dom'
 import Routes from './Routes'
 import Logo from '../components/template/Logo'
