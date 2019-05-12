5719f7e28725387b1c0514c02f924c058b499d57
dutraneto <dudutraneto@gmail.com>
2019-05-12 16:19:36 -0400

    Updates

---
+++ precache-manifest.488e6e8d7bc492b0d01678465af5358f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "89e2666c24d37055bcb60e9d2d9f7e35",
+    "url": "/react-crud/frontend/build/static/media/Roboto-Thin.89e2666c.ttf"
+  },
+  {
+    "revision": "c0c39ae7bc30513c9862",
+    "url": "/react-crud/frontend/build/static/css/main.afb1df4c.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-crud/frontend/build/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "127b35add13978df55b2",
+    "url": "/react-crud/frontend/build/static/js/1.127b35ad.chunk.js"
+  },
+  {
+    "revision": "adf70f5872511ef6a16b",
+    "url": "/react-crud/frontend/build/static/js/runtime~main.adf70f58.js"
+  },
+  {
+    "revision": "8cd1c759964a6c1f0e25a86cbc4fa6da",
+    "url": "/react-crud/frontend/build/static/media/logo.8cd1c759.png"
+  },
+  {
+    "revision": "c0c39ae7bc30513c9862",
+    "url": "/react-crud/frontend/build/static/js/main.c0c39ae7.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-crud/frontend/build/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-crud/frontend/build/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-crud/frontend/build/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-crud/frontend/build/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "127b35add13978df55b2",
+    "url": "/react-crud/frontend/build/static/css/1.467231a7.chunk.css"
+  },
+  {
+    "revision": "0990c32cb8fbecef4d862ccb9991650e",
+    "url": "/react-crud/frontend/build/index.html"
+  }
+];
