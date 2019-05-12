17d01c681d720bcf48bb260917e124dc4cb06321
dutraneto <dudutraneto@gmail.com>
2019-05-12 16:11:33 -0400

    fix typo

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -6,7 +6,7 @@ export default props =>
     <Main icon="home" title="start" subtitle="2nd Project of React Studies">
         <div className="display-4">Welcome!</div>
         <hr/>
-        <p className="mb-0">The porpuse of this system is to exemplify
+        <p className="mb-0">The purpose of this system is to exemplify
             a register developed using React.
         </p>
     </Main>
